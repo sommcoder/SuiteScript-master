d1513e040d79898f007c00cb727c4ea81e407210
Brian Davies <brian.davies589@gmail.com>
2022-04-19 17:27:02 -0400

    asd

--- client-script-2.js
+++ client-script-2.js
@@ -8,52 +8,131 @@
  *@NApiVersion 2.x
  *@NScriptType ClientScript
  */
-define(["N/ui/dialog"], function (dialog) {
+define(["N/ui/dialog"], function (ui) {
   // record and currentrecord modules are auto loaded with a Client Script declared in the JS Docs apparently???
 
-  function fieldChanged(context) {
+  // variable list
+  var currEmpRecord;
+  var departmentField;
+  var locationField;
+  var classField;
+  var departmentValue;
+  var locationValue;
+  var classValue;
+
+  function pageInit(context) {
     if (context.mode !== "edit") return;
+    try {
+      currEmpRecord = context.currentRecord;
+      // department field
+      departmentField = currEmpRecord.getField({
+        fieldId: "department",
+      });
+      departmentField.isDisabled = false;
 
-    var currEmpRecord = context.currentRecord;
-    console.log(currEmpRecord);
-    var department = currEmpRecord.getValue({
-      fieldId: "department",
-    });
-    var _class = currEmpRecord.getValue({
-      fieldId: "class",
-    });
-    var location = currEmpRecord.getValue({
-      fieldId: "location",
-    });
+      // class field
+      classField = currEmpRecord.getField({
+        fieldId: "class",
+      });
+      classField.isDisabled = true;
 
-    console.log(department, _class, location);
+      // location field
+      locationField = currEmpRecord.getField({
+        fieldId: "location",
+      });
+      console.log(location);
+      locationField.isDisabled = true;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  function validateField(context) {
-    var contextType = context.type;
-    var contextName = context.name;
-    var contextLinenum = context.linenum;
-    console.log(contextType, contextName, contextLinenum);
+  function fieldChanged(context) {
+    console.log(context.fieldId);
+    if (
+      context.fieldId !== "department" &&
+      context.fieldId !== "class" &&
+      context.fieldId !== "location"
+    )
+      return;
 
-    // if (context.mode !== "edit") return;
-    // validateField requires a boolean return
-    // if (department !== entered) return alert("You must enter a department first");
-    // if (engineering || sales)
-    //     classes = 'consulting and project management'
-    //     else {classes = 'Personnel';
-    //         location = anything BUT Century City
-    // }
-    // if (classes !== entered) return alert("you must enter a class first");
-    // if (classes === "consulting and project management")
-    //   location = "Century City";
-    // if (location !== entered) return alert("you must enter a class first");
+    try {
+      currEmpRecord = context.currentRecord;
+      // record fields:
+      departmentField = currEmpRecord.getField({
+        fieldId: "department",
+      });
+      classField = currEmpRecord.getField({
+        fieldId: "class",
+      });
+      locationField = currEmpRecord.getField({
+        fieldId: "location",
+      });
+
+      // record values:
+      departmentValue = currEmpRecord.getValue({
+        fieldId: "department",
+      });
+      classValue = currEmpRecord.getValue({
+        fieldId: "class",
+      });
+      locationValue = currEmpRecord.getValue({
+        fieldId: "location",
+      });
+
+      console.log(departmentValue);
+      console.log(classValue);
+      console.log(locationValue);
+
+      departmentValue ? (classField.isDisabled = false) : "";
+      classValue ? (locationField.isDisabled = false) : "";
+
+      // ...and location can be anything BUT Century City
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   function saveRecord(context) {
-    // always return a boolean
+    if (context.currentRecord !== "employee") return;
+    try {
+      currEmpRecord = context.currentRecord;
+      // record values:
+      departmentValue = currEmpRecord.getValue({
+        fieldId: "department",
+      });
+      classValue = currEmpRecord.getValue({
+        fieldId: "class",
+      });
+      locationValue = currEmpRecord.getValue({
+        fieldId: "location",
+      });
+
+      if (
+        departmentValue !== true &&
+        classValue !== true &&
+        locationValue !== true
+      )
+        alert(
+          "you need to fill out the department, class & location fields before submitting!"
+        );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function validateField(context) {
+    try {
+      currEmpRecord = context.currentRecord;
+      var field = context.fieldId;
+      if (field === "department") console.log(field);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return {
+    pageInit: pageInit,
     saveRecord: saveRecord,
     validateField: validateField,
     fieldChanged: fieldChanged,
