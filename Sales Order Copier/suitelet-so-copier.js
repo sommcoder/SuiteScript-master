ae98bac1a7c9d21ac221e2894aa2b14f909b429e
Brian Davies <brian.davies589@gmail.com>
2022-07-06 11:59:34 -0400

    asd

--- Sales Order Copier/suitelet-so-copier.js
+++ Sales Order Copier/suitelet-so-copier.js
@@ -29,317 +29,349 @@ define([
   "N/url",
   "N/https",
 ], function (search, ui, record, url, https, crypto) {
-  // global variables
+  // general global variables:
   let reqParams;
-  let recordId;
+  //   let recordId;
   let recordType;
   let ogRecordTranId;
 
+  // global sublist array variables:
+  let sublistLines;
+  const sublistFieldValuesArr = [];
+
   function onRequest(context) {
     try {
-      if (context.request.method !== "GET") return;
-
-      log.debug({
-        title: "passed GET?:",
-        details: "yes!",
-      });
-
-      log.debug({
-        title: "context:",
-        details: context.request,
-      });
-
-      // global variable assignments:
-      reqParams = context.request.parameters;
-      recordType = reqParams.currRecordType;
-      recordId = reqParams.currRecordId;
-      ogRecordTranId = reqParams.recordTranId;
-
-      log.debug({
-        title: "req params:",
-        details: reqParams,
-      });
-
-      log.debug({
-        title: "tran id:",
-        details: ogRecordTranId,
-      });
-
-      const sublistItems = JSON.parse(reqParams.sublistValuesArr);
-
-      // create the form on the script:
-      const salesOrderCopyForm = ui.createForm({
-        title: "Copy Order",
-        hideNavBar: true,
-      });
-
-      ///////// display Order FieldGroup: /////////
-      const customerDetailsFieldGroup = salesOrderCopyForm.addFieldGroup({
-        id: "custpage_customer_details_field_group",
-        label: "Info to copy:",
-      });
-
-      const customerField = salesOrderCopyForm.addField({
-        id: "entity_field",
-        label: "Customer",
-        type: "text",
-        container: "custpage_customer_details_field_group",
-      });
-
-      const subsidiaryField = salesOrderCopyForm.addField({
-        id: "subsidiary_field",
-        label: "Subsidiary",
-        type: "text",
-        container: "custpage_customer_details_field_group",
-      });
-
-      const locationField = salesOrderCopyForm.addField({
-        id: "location_field",
-        label: "Location",
-        type: "select",
-        container: "custpage_customer_details_field_group",
-      });
-
-      const departmentField = salesOrderCopyForm.addField({
-        id: "department_field",
-        label: "Department",
-        type: "select",
-        container: "custpage_customer_details_field_group",
-      });
-
-      const classField = salesOrderCopyForm.addField({
-        id: "class_field",
-        label: "Class",
-        type: "select",
-        container: "custpage_customer_details_field_group",
-      });
-
-      ////////// item details sublist: ///////
-
-      const itemDetailsSublist = salesOrderCopyForm.addSublist({
-        id: "custpage_item_details_sublist",
-        label: "Items to copy:",
-        type: "list",
-      });
-      itemDetailsSublist.addMarkAllButtons();
-
-      const checkbox = itemDetailsSublist.addField({
-        id: "custpage_checkbox",
-        label: "select",
-        type: "checkbox",
-      });
-      checkbox.defaultValue = "T";
-
-      const itemField = itemDetailsSublist.addField({
-        id: "item_field",
-        label: "Item",
-        type: "text",
-      });
-
-      const rateField = itemDetailsSublist.addField({
-        id: "rate_field",
-        label: "Rate",
-        type: "text",
-      });
-
-      const quantityField = itemDetailsSublist.addField({
-        id: "quantity_field",
-        label: "Quantity",
-        type: "text",
-      });
-
-      const shipdateField = itemDetailsSublist.addField({
-        id: "shipdate_field",
-        label: "Shipdate",
-        type: "text",
-      });
-
-      let entityId; // global variable, used on Record.copy
-      let i = 0; // counter variable for search
-
-      search
-        .create({
-          type: recordType,
-          filters: [["item", "anyof", sublistItems]],
-          columns: [
-            { name: "entity" },
-            { name: "subsidiary" },
-            { name: "location" },
-            { name: "department" },
-            { name: "class" },
-            { name: "item" },
-            { name: "rate" },
-            { name: "quantity" },
-            { name: "shipdate" },
-          ],
-        })
-        .run()
-        .each((result) => {
-          entityId = result.getValue({
-            name: "entity",
-          });
-
-          //non-select fields:
-          customerField.defaultValue = result.getText({
-            name: "entity",
-          });
-
-          subsidiaryField.defaultValue = result.getText({
-            name: "subsidiary",
-          });
-          // select fields:
-          locationField.addSelectOption({
-            value: result.getValue({
-              name: "location",
-            }),
-            text: result.getText({
-              name: "location",
-            }),
-          });
-
-          departmentField.addSelectOption({
-            value: result.getValue({
-              name: "department",
-            }),
-            text: result.getText({
-              name: "department",
-            }),
-          });
-
-          classField.addSelectOption({
-            value: result.getValue({
-              name: "class",
-            }),
-            text: result.getText({
-              name: "class",
-            }),
-          });
-
-          //////// item details sublist ///////
-
-          itemDetailsSublist.setSublistValue({
-            id: "item_field",
-            line: i,
-            value: result.getText({
-              name: "item",
-            }),
-          });
-          itemDetailsSublist.setSublistValue({
-            id: "rate_field",
-            line: i,
-            value: result.getValue({
-              name: "rate",
-            }),
-          });
-          itemDetailsSublist.setSublistValue({
-            id: "quantity_field",
-            line: i,
-            value: result.getValue({
-              name: "quantity",
-            }),
-          });
-
-          itemDetailsSublist.setSublistValue({
-            id: "shipdate_field",
-            line: i,
-            value:
-              result.getValue({
-                name: "shipdate",
-              }) || 0,
-          });
-
-          i++;
-
-          return true;
+      if (context.request.method == "GET") {
+        log.debug({
+          title: "passed GET?:",
+          details: "yes!",
         });
 
-      const copiedRecord = record.copy({
-        type: recordType,
-        id: recordId,
-        defaultValues: {
-          entity: entityId,
-        },
-      });
+        log.debug({
+          title: "context:",
+          details: context.request,
+        });
 
-      copiedRecord.save();
+        // global variable assignments:
+        reqParams = context.request.parameters;
+        recordType = reqParams.currRecordType;
+        recordId = reqParams.currRecordId;
+        ogRecordTranId = reqParams.recordTranId;
 
-      log.debug({
-        title: "Copied record object:",
-        details: copiedRecord,
-      });
+        log.debug({
+          title: "req params:",
+          details: reqParams,
+        });
 
-      // NS auto populates the sales order record upon saving...
+        log.debug({
+          title: "tran id:",
+          details: ogRecordTranId,
+        });
 
-      const submitButton = salesOrderCopyForm.addSubmitButton({
-        label: "Submit Record",
-      });
+        const sublistItems = JSON.parse(reqParams.sublistValuesArr);
 
-      log.debug({
-        title: "sublist:",
-        details: itemDetailsSublist,
-      });
+        // create the form on the script:
+        const salesOrderCopyForm = ui.createForm({
+          title: "Copy Order",
+          hideNavBar: true,
+        });
 
-      // WRITE Suitelet Page:
-      context.response.writePage({
-        pageObject: salesOrderCopyForm,
-      });
+        ///////// display Order FieldGroup: /////////
+        const customerDetailsFieldGroup = salesOrderCopyForm.addFieldGroup({
+          id: "custpage_customer_details_field_group",
+          label: "Info to copy:",
+        });
 
-      if (context.request.method !== "POST") return;
-      // when form is submitted, a POST request comes through
+        const customerField = salesOrderCopyForm.addField({
+          id: "entity_field",
+          label: "Customer",
+          type: "text",
+          container: "custpage_customer_details_field_group",
+        });
 
+        const subsidiaryField = salesOrderCopyForm.addField({
+          id: "subsidiary_field",
+          label: "Subsidiary",
+          type: "text",
+          container: "custpage_customer_details_field_group",
+        });
 
-      context.ServerRequest.getSublistValue({
-        group: 'custpage_item_details_sublist',
-        line: '0',
-        name: string*
-      })
+        const locationField = salesOrderCopyForm.addField({
+          id: "inpt_location_field",
+          label: "Location",
+          type: "select",
+          container: "custpage_customer_details_field_group",
+        });
 
-      log.debug({
-        title: "request url:",
-        details: context.request.url,
-      });
+        const departmentField = salesOrderCopyForm.addField({
+          id: "inpt_department_field",
+          label: "Department",
+          type: "select",
+          container: "custpage_customer_details_field_group",
+        });
 
-      log.debug({
-        title: "record type else statement:",
-        details: recordType,
-      });
-      log.debug({
-        title: "POST context:",
-        details: context,
-      });
+        const classField = salesOrderCopyForm.addField({
+          id: "inpt_class_field",
+          label: "Class",
+          type: "select",
+          container: "custpage_customer_details_field_group",
+        });
 
-      log.debug({
-        title: "POST context req params:",
-        details: context.request.parameters,
-      });
+        ////////// item details sublist: ///////
 
-      log.debug({
-        title: "POST body:",
-        details: context.request.body,
-      });
+        itemDetailsSublist = salesOrderCopyForm.addSublist({
+          id: "custpage_item_details_sublist",
+          label: "Items to copy:",
+          type: "list",
+        });
+        itemDetailsSublist.addMarkAllButtons();
 
-      const headers = new Array();
-      // use destructuring to populate array with key/value pairs!
-      headers["Content-Type"] = "application/json";
-      headers["User-Agent-x"] = "SuiteScript Call";
+        const checkboxField = itemDetailsSublist.addField({
+          id: "checkbox_field",
+          label: "select",
+          type: "checkbox",
+        });
+        checkboxField.defaultValue = "T";
 
-      log.debug({
-        title: "url?:",
-        details: context.request.url,
-      });
+        const itemField = itemDetailsSublist.addField({
+          id: "item_field",
+          label: "Item",
+          type: "text",
+        });
 
-      log.debug({
-        title: "recordType",
-        details: recordType,
-      });
+        const rateField = itemDetailsSublist.addField({
+          id: "rate_field",
+          label: "Rate",
+          type: "text",
+        });
 
-      //   context.response.sendRedirect({
-      //     identifier: recordType,
-      //     type: "RECORD",
-      //     editMode: false,
-      //     id: number | string,
-      //     parameters: {},
-      //   });
+        const quantityField = itemDetailsSublist.addField({
+          id: "quantity_field",
+          label: "Quantity",
+          type: "text",
+        });
+
+        const shipdateField = itemDetailsSublist.addField({
+          id: "shipdate_field",
+          label: "Shipdate",
+          type: "text",
+        });
+
+        let entityId; // global variable, used on Record.copy
+        let i = 0; // counter variable for search
+
+        search
+          .create({
+            type: recordType,
+            filters: [["item", "anyof", sublistItems]],
+            columns: [
+              { name: "entity" },
+              { name: "subsidiary" },
+              { name: "location" },
+              { name: "department" },
+              { name: "class" },
+              { name: "item" },
+              { name: "rate" },
+              { name: "quantity" },
+              { name: "shipdate" },
+            ],
+          })
+          .run()
+          .each((result) => {
+            entityId = result.getValue({
+              name: "entity",
+            });
+
+            //non-select fields:
+            customerField.defaultValue = result.getText({
+              name: "entity",
+            });
+
+            subsidiaryField.defaultValue = result.getText({
+              name: "subsidiary",
+            });
+            // select fields:
+            locationField.addSelectOption({
+              value: result.getValue({
+                name: "location",
+              }),
+              text: result.getText({
+                name: "location",
+              }),
+            });
+
+            departmentField.addSelectOption({
+              value: result.getValue({
+                name: "department",
+              }),
+              text: result.getText({
+                name: "department",
+              }),
+            });
+
+            classField.addSelectOption({
+              value: result.getValue({
+                name: "class",
+              }),
+              text: result.getText({
+                name: "class",
+              }),
+            });
+
+            //////// item details sublist ///////
+
+            itemDetailsSublist.setSublistValue({
+              id: "item_field",
+              line: i,
+              value: result.getText({
+                name: "item",
+              }),
+            });
+            itemDetailsSublist.setSublistValue({
+              id: "rate_field",
+              line: i,
+              value: result.getValue({
+                name: "rate",
+              }),
+            });
+            itemDetailsSublist.setSublistValue({
+              id: "quantity_field",
+              line: i,
+              value: result.getValue({
+                name: "quantity",
+              }),
+            });
+
+            itemDetailsSublist.setSublistValue({
+              id: "shipdate_field",
+              line: i,
+              value:
+                result.getValue({
+                  name: "shipdate",
+                }) || 0,
+            });
+
+            i++;
+
+            return true;
+          });
+
+        log.debug({
+          title: " itemDetailsSublist",
+          details: itemDetailsSublist,
+        });
+
+        sublistLines = itemDetailsSublist.lineCount;
+
+        sublistFieldArr = [
+          checkboxField.id,
+          itemField.id,
+          rateField.id,
+          shipdateField.id,
+        ];
+
+        log.debug({
+          title: "sublistLines",
+          details: sublistLines,
+        });
+
+        log.debug({
+          title: "sublistFieldArr",
+          details: sublistFieldArr,
+        });
+
+        // NS auto populates the sales order record upon saving...
+
+        const submitButton = salesOrderCopyForm.addSubmitButton({
+          label: "Submit Record",
+        });
+
+        // WRITE Suitelet Page:
+        context.response.writePage({
+          pageObject: salesOrderCopyForm,
+        });
+      } else {
+        // when form is submitted, a POST request is called when the submitButton is clicked!
+
+        log.debug({
+          title: "context:",
+          details: context.request,
+        });
+
+        log.debug({
+          title: "POST parameters:",
+          details: context.request.parameters,
+        });
+
+        log.debug({
+          title: "POST body:",
+          details: context.request.body,
+        });
+
+        //Form: Customer Details values:
+
+        const classFieldValue = context.request.parameters.inpt_class_field;
+
+        const departmentFieldValue =
+          context.request.parameters.inpt_department_field;
+
+        const locationFieldValue =
+          context.request.parameters.inpt_location_field;
+
+        // Form: Item Details Sublist Values:
+        // using global variables/arrays:
+        // loop through item sublist
+        for (let i = 0; i < sublistFieldArr.length; i++) {
+          for (let j = 0; j < sublistLines.length; j++) {
+            result_[i] = context.ServerRequest.getSublistValue({
+              group: "custpage_item_details_sublist",
+              line: j,
+              name: i,
+            });
+            sublistFieldValuesArr.push(result_[i]);
+          }
+        }
+
+        // copy original record and adjust values!
+
+        log.debug({
+          title: "sublistFieldValuesArr:",
+          details: sublistFieldValuesArr,
+        });
+
+        const copiedRecord = record.copy({
+          type: recordType,
+          id: recordId,
+          defaultValues: {
+            entity: entityId,
+          },
+        });
+
+        copiedRecord.save();
+
+        log.debug({
+          title: "Copied record object:",
+          details: copiedRecord,
+        });
+
+        // const headers = new Array();
+        // // use destructuring to populate array with key/value pairs!
+        // headers["Content-Type"] = "application/json";
+        // headers["User-Agent-x"] = "SuiteScript Call";
+
+        // log.debug({
+        //   title: "url?:",
+        //   details: context.request.url,
+        // });
+
+        //   context.response.sendRedirect({
+        //     identifier: recordType,
+        //     type: "RECORD",
+        //     editMode: false,
+        //     id: number | string,
+        //     parameters: {},
+        //   });
+      }
     } catch (err) {
       log.debug({
         title: "Error:",
