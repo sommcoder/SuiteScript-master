cb3778ec2f028fd749597248e5246b21b1fbcf5c
Brian Davies <brian.davies589@gmail.com>
2022-08-19 18:12:24 -0400

    some refactoring. Encapsulating the code into two primary functions with conditions on when they should be triggered. This makes the code more readable

--- Lot Number Challenge/lotNumberUEScript.js
+++ Lot Number Challenge/lotNumberUEScript.js
@@ -1,7 +1,7 @@
 /* 
 on save of a SALES ORDER, create Purchase Order for the items and quantities listed on that order
 
-The items on the order must be Lot Numbered items (look into this, check if the account has any)
+The items on the purchase order must be Lot Numbered items (look into this, check if the account has any)
 
 Link the PO to the Sales Order through a field and VICE VERSA (two-way link)
 
@@ -17,12 +17,21 @@ Think Amazon for products they don't MAKE themselves. They purchase it from the 
     // hard-code a VENDER on the Purchase Order
 
 
---------- Summary: ----------
+--------- Summary: --------------
 1) On save, create new Purchase order
 2) auto-populate the items and quantities from sales order into the PO
    the items on the SO MUST be LOT NUMBERED.
 3) have the PO and SO linked through a field (two way link)... the PO field?
 4) On receipt of the PO, add the LOTs received onto the INVENTORY DETAIL of the SALES ORDERs
+
+
+----------- Use Cases: ----------
+
+1) user doesn't choose the lot number custom form
+2) SO with no lot numbered items
+3) PO location gets changes
+4) PO item gets deleted
+5) SO line gets deleted
 
 */
 
@@ -33,56 +42,37 @@ Think Amazon for products they don't MAKE themselves. They purchase it from the 
 define(["N/record", "N/search"], function (record, search) {
   function afterSubmit(context) {
     try {
-      /* 
-      -SO and PO are linked through a custom field
-      -now that we have our script deploying on sales order and item receipt records
-      */
       const currRecord = context.newRecord;
       const currRecordId = currRecord.id;
       const currRecordType = currRecord.type;
       const contextType = context.type;
+      // global variable:
+      let lotNumberedItem;
 
-      // need access to this variable in the item receipt conditional block:
+      //--------------- Sales Orders Block ------------------------------//
+      if (currRecordType === "salesorder" && contextType === "create") {
+        createPO();
+      }
 
-      //---------------Sales Order CONDITIONAL BLOCK:
-      if (currRecordType === "salesorder") {
-        if (contextType !== "create") return;
-        const custForm = currRecord.getValue({
-          fieldId: "customform",
+      //---------------- Item Receipt Block---------------------------------//
+      if (currRecordType === "itemreceipt") {
+        setLots();
+      }
+
+      // declarations get hoisted to the top of the block
+      // function declaration 1:
+      function createPO() {
+        let poRecord;
+        let soSublistValue;
+        let customer;
+        let bodyFieldValue;
+
+        /////////////////////////////////////////////////////////////////////
+
+        const skippedIndices_SO = [];
+        const numLines = currRecord.getLineCount({
+          sublistId: "item",
         });
-
-        // Custom Form guard clause:
-        if (custForm !== "340") return;
-
-        log.debug({
-          title: "newRecordId:",
-          details: currRecordId,
-        });
-
-        // Preliminary check to see if lot numbered items even exist
-        let lotNumbers = [];
-
-        for (let l = 0; l < numLines; l++) {
-          lotNumberedItem = currRecord.getSublistValue({
-            sublistId: "item",
-            fieldId: "isnumbered",
-            line: l,
-          });
-          lotNumberedItem === "T" ? lotNumbers.push("T") : lotNumbers.push("F");
-        }
-
-        let lotNumberedItemsCheck = lotNumbers.some((el) => {
-          el === "T";
-        });
-
-        let poRecord; // we want the poRecord to be available globally
-        // the poRecord is created only IF there is at least ONE lot numbered item within the SO item sublist
-        if (lotNumberedItemsCheck) {
-          poRecord = record.create({
-            type: "purchaseorder",
-          });
-        }
-        /////////////////////////////////////
         const sharedSublistFieldsArr = [
           "item",
           "quantity",
@@ -91,18 +81,8 @@ define(["N/record", "N/search"], function (record, search) {
           "amount",
         ];
 
-        const numLines = currRecord.getLineCount({
-          sublistId: "item",
-        });
-
-        const skippedIndices_SO = [];
-        let invDetail_IR;
-        let lotNumberedItem;
-        let lineKeyNumber;
-
-        // ITEMs must be LOT NUMBERED
-
         // ITEM SUBLIST LINE LOOP:
+        // SETS the lot numbered line items on the newly created PO Record
         for (let l = 0; l < numLines; l++) {
           lotNumberedItem = currRecord.getSublistValue({
             sublistId: "item",
@@ -111,56 +91,20 @@ define(["N/record", "N/search"], function (record, search) {
           });
 
           if (lotNumberedItem === "T") {
-            invDetail_IR = currRecord.getSublistSubrecord({
-              sublistId: "item",
-              fieldId: "inventorydetail",
-              line: l,
-            });
-
-            log.debug({
-              title: "invDetail_IR",
-              details: invDetail_IR,
-            });
-
-            lineKeyNumber = currRecord.getSublistValue({
-              sublistId: "item",
-              fieldId: "lineuniquekey",
-              line: l,
-            });
-
-            log.debug({
-              title: "lineKeyNumber:",
-              details: lineKeyNumber,
-            });
-
-            poRecord.setSublistValue({
-              sublistId: "item",
-              fieldId: "custcol18",
-              line: l,
-              value: lineKeyNumber,
-            });
-
-            /*
-
-    skippedIndices_SO[0] ? skippedIndices_SO[0] : l   
-
-    explanation:
-    if there is a value in the array, give that value, if not, just give the index value of l
-
-         */
+            // if there is no value on the PO record variable yet, create and set it
+            if (!poRecord) {
+              poRecord = record.create({
+                type: "purchaseorder",
+              });
+            }
 
             for (let i = 0; i < sharedSublistFieldsArr.length; i++) {
-              let soSublistValue =
+              soSublistValue =
                 currRecord.getSublistValue({
                   sublistId: "item",
                   fieldId: sharedSublistFieldsArr[i],
                   line: l,
                 }) || 0;
-
-              log.debug({
-                title: "soSublistValue from l:",
-                details: [sharedSublistFieldsArr[i], soSublistValue, "FROM", l],
-              });
 
               poRecord.setSublistValue({
                 sublistId: "item",
@@ -170,7 +114,7 @@ define(["N/record", "N/search"], function (record, search) {
               });
 
               // link SO ENTITY, set as CUSTOMER on each item line in PO:
-              const customer = currRecord.getValue({
+              customer = currRecord.getValue({
                 fieldId: "entity",
               });
 
@@ -202,8 +146,9 @@ define(["N/record", "N/search"], function (record, search) {
             skippedIndices_SO.push(l);
           }
         }
-
-        const sharedFields = [
+        /////////////////////////////////////////////////////////////////////
+        // BODY FIELDS: get and set
+        const sharedBodyFieldsArr = [
           "tranid",
           "trandate",
           "entity",
@@ -214,77 +159,46 @@ define(["N/record", "N/search"], function (record, search) {
           "location",
         ];
 
-        // BODY FIELDS:
-        for (let i = 0; i < sharedFields.length; i++) {
-          const gotValue = currRecord.getValue({
-            fieldId: sharedFields[i],
+        for (let i = 0; i < sharedBodyFieldsArr.length; i++) {
+          bodyFieldValue = currRecord.getValue({
+            fieldId: sharedBodyFieldsArr[i],
           });
           poRecord.setValue({
-            fieldId: sharedFields[i],
-            value: gotValue,
+            fieldId: sharedBodyFieldsArr[i],
+            value: bodyFieldValue,
           });
         }
-
-        // unique PO fields:
+        /////////////////////////////////////////////////////////////////////////
+        // UNIQUE PO Fields:
         poRecord.setValue({
           fieldId: "entity", // entity = Vendor in a PO
           value: 1840, // Adam Smith CPA hard-coded Vendor
         });
-
         // set the CustomForm on the PO record BEFORE saving it
         poRecord.setValue({
           fieldId: "customform",
-          value: "341",
+          value: "341", // hard coded so that we can set the value of "custbody14" below
         });
-
         // set the custom field on the PO to be the link to the SO
         poRecord.setValue({
           fieldId: "custbody14",
           value: currRecordId,
         });
-
+        /////////////////////////////////////////////////////////////////////////////////
         const poRecordId = poRecord.save();
 
-        log.debug({
-          title: "PO Record AFTER Sublist Loop:",
-          details: poRecord,
-        });
-
-        log.debug({
-          title: "poRecordId:",
-          details: poRecordId,
-        });
-
-        // submit Fields to get the SO record
-        const soRecord = record.submitFields({
+        // submit the linked SO Field separately
+        record.submitFields({
           type: "salesorder",
           id: currRecordId,
           values: {
             custbody14: poRecordId,
           },
         });
-
-        log.debug({
-          title: "soRecordId:",
-          details: soRecord.id,
-        });
       }
 
-      //---------------- Item Receipt CONDITIONAL BLOCK:
-      if (currRecordType === "itemreceipt") {
-        log.debug({
-          title: "currRecord itemReceipt:",
-          details: currRecord,
-        });
-        log.debug({
-          title: "currRecordType itemReceipt:",
-          details: currRecordType,
-        });
-        log.debug({
-          title: "context Type itemReceipt:",
-          details: contextType,
-        });
-
+      // function declaration 2:
+      function setLots() {
         const soRecordId = search.lookupFields({
           type: "purchaseorder",
           id: currRecord.getValue({
@@ -293,37 +207,17 @@ define(["N/record", "N/search"], function (record, search) {
           columns: "custbody14",
         });
 
-        log.debug({
-          title: "SO Search Result:",
-          details: soRecordId,
-        });
-
-        log.debug({
-          title: "SO Search Result:",
-          details: soRecordId.custbody14[0].value,
-        });
-
-        //////////////////////////////////////////////////////////////////////
-        // This is ONE of the few exceptions where we HAVE to use record.load
-        //////////////////////////////////////////////////////////////////////
         const soRecord = record.load({
           type: "salesorder",
           id: soRecordId.custbody14[0].value,
         });
 
-        // this is assuming the # of lines on the PO is the same on the SO, which it SHOULD be since the SO creates the PO:
-
-        log.debug({
-          title: "Item Receipt id:",
-          details: currRecordId,
-        });
-
         // search global variables:
         let invDetail_SO;
 
-        // item grouping variables:
-        const lotNumberItemGroups = {};
-        let lnKey_SO, lotId, quantity, lotLocation;
+        // Object variables:
+        const lotNumberItemObj = {};
+        let lnKey_SO, lotId, quantity, itemLocation;
 
         search
           .create({
@@ -373,30 +267,17 @@ define(["N/record", "N/search"], function (record, search) {
             });
 
             // if no item object exists yet, create one and assign it to an object that contains the location and lots properties, location is a number and lots is an empty array that will be filled
-            if (!lotNumberItemGroups[lnKey_SO])
-              lotNumberItemGroups[lnKey_SO] = {
+            if (!lotNumberItemObj[lnKey_SO])
+              lotNumberItemObj[lnKey_SO] = {
                 location: itemLocation,
                 lots: [],
               };
 
-            // if (!lotNumberItemGroups[lnKey_SO].location) {
-            //   lotNumberItemGroups[lnKey_SO].location = lotLocation;
-            // }
-
-            log.debug({
-              title: "lotNumberItemGroups",
-              details: lotNumberItemGroups,
-            });
-
-            lotNumberItemGroups[lnKey_SO].lots.push({
+            lotNumberItemObj[lnKey_SO].lots.push({
               lot: lotId,
               quantity: quantity,
             });
 
-            log.debug({
-              title: "lotNumberItemGroups:",
-              details: lotNumberItemGroups,
-            });
             return true;
           });
 
@@ -406,13 +287,8 @@ define(["N/record", "N/search"], function (record, search) {
           sublistId: "item",
         });
 
-        log.debug({
-          title: "soItemLineCount",
-          details: soItemLineCount,
-        });
-
         // keys = array of the lineKeys
-        let keys = Object.keys(lotNumberItemGroups);
+        let keys = Object.keys(lotNumberItemObj);
         let soLineKey;
         let itemKeysValues;
         let irLocation;
@@ -426,19 +302,11 @@ define(["N/record", "N/search"], function (record, search) {
 
         // ITEM SUBLIST LINE LOOP:
         for (let l = 0; l < soItemLineCount; l++) {
-          log.debug({
-            title: "SO item sublist line:",
-            details: ["SO item line:", l],
-          });
           // Determine if the line item is LOT NUMBERED:
           lotNumberedItem = soRecord.getSublistValue({
             sublistId: "item",
             fieldId: "isnumbered",
             line: l,
-          });
-          log.debug({
-            title: "lotNumberedItem?:",
-            details: lotNumberedItem,
           });
 
           soLineKey = soRecord.getSublistValue({
@@ -455,32 +323,10 @@ define(["N/record", "N/search"], function (record, search) {
           if (lotNumberedItem === "T" && keys.includes(soLineKey)) {
             // if the previous conditions are valid we're good to do the following procedures and check for ir location to match the so location
             // this reassigns the values array of the item that we are going to loop through
-            itemKeysValues = Object.values(
-              lotNumberItemGroups[soLineKey]
-            ).flat();
+            itemKeysValues = Object.values(lotNumberItemObj[soLineKey]).flat();
 
             // splice out the location value which mutates the original array to
             irLocation = itemKeysValues.splice(0, 1);
-
-            log.debug({
-              title: "irLocation",
-              details: irLocation,
-            });
-
-            log.debug({
-              title: "itemKeysValues LENGTH:",
-              details: itemKeysValues.length,
-            });
-
-            log.debug({
-              title: "itemKeysValues:",
-              details: itemKeysValues,
-            });
-
-            log.debug({
-              title: "soLocation",
-              details: soLocation,
-            });
 
             // if the irLocation of the item DOESN'T match the soLocation, continue to the next iteration!
             // the value of irLocation gets type-coerced before their equality is checked
@@ -497,18 +343,6 @@ define(["N/record", "N/search"], function (record, search) {
               sublistId: "item",
               fieldId: "inventorydetail",
               line: l,
-            });
-
-            log.debug({
-              title: "invDetail_SO:",
-              details: [
-                "item sublist line:",
-                l,
-                "record:",
-                invDetail_SO,
-                "soLineKey:",
-                soLineKey,
-              ],
             });
 
             // if (itemKeysValues[n].location !== soLocation) continue;
@@ -555,10 +389,6 @@ define(["N/record", "N/search"], function (record, search) {
                 ],
               });
             }
-            /*
-            AFTER a successful loop is complete we go back to the line loop at the top of the block
-            We iterate our i counter to move on to the NEXT element in our constructed object
-            */
           } else {
             log.debug({
               title: "skipped line #",
@@ -575,7 +405,6 @@ define(["N/record", "N/search"], function (record, search) {
       });
     }
   }
-
   return {
     afterSubmit: afterSubmit,
   };
